13e3f3e8dc2acbab975eb024227fcccadd2a6bce
charles-hsuz <charles_hsu@qq.com>
2023-02-03 02:37:03 +0800

    Site updated: 2023-02-03 02:36:52

--- js/costum.js
+++ js/costum.js
@@ -1,5 +1,23 @@
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
 var old_title = document.title;
 var time_count;
+
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         time_count = setTimeout(function () {
@@ -12,16 +30,20 @@ document.addEventListener('visibilitychange', function () {
     }
 });
 
-function getConfirmation() {
-    var hr = location.href;
-    var retVal = confirm("你想和我一起放烟花吗？");
-    if (retVal == true) {
-        location.href = hr + "/fire.html";
-        return;
+// 烟花
+function getFireConfirm() {
+    if(getCookie("noFire") == "true") return;
+    if (confirm("你想和我一起放烟花吗？")) {
+        location.href += "/fire.html";
     } else {
-        return;
+        if (confirm("7天内不再提醒？")) {
+            setCookie("noFire", "true", 7);
+        }
     }
+    return;
 }
 
-getConfirmation();
+getFireConfirm();
+
+
 
