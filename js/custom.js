09803f40327edecfeb32319d4477d5554db3cfa0
charles-hsuz <charles_hsu@qq.com>
2023-02-15 10:57:08 +0800

    Site updated: 2023-02-15 10:56:43

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
-const directoryPath = '/js/custom/';
-const files = ['fire.js', 'ascii.js', 'ex-search.js', 'installPWA.js', 'music.js'];
-files.forEach(function (file) {
+let directoryPath = '/js/custom/';
+const p_files = ['fire.js', 'ex-search.js', 'installPWA.js'];
+p_files.forEach(function (file) {
   if (file.endsWith('.js')) {
     const script = document.createElement('script');
     script.setAttribute('data-pjax', '')
@@ -8,4 +8,13 @@ files.forEach(function (file) {
     script.src = directoryPath + file;
     document.head.appendChild(script);
   }
-});
+})
+const files = ['music.js', 'ascii.js', 'PublicSacrificeDay.js'];
+files.forEach(function (file) {
+  if (file.endsWith('.js')) {
+    const script = document.createElement('script');
+    script.setAttribute('async', '')
+    script.src = directoryPath + file;
+    document.head.appendChild(script);
+  }
+});;
