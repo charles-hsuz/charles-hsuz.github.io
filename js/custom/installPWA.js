323b954cb84a2716ff2bc8a611750098379245b8
charles-hsuz <charles_hsu@qq.com>
2023-02-15 11:39:25 +0800

    Site updated: 2023-02-15 11:39:24

--- js/custom/installPWA.js
+++
@@ -1,4 +0,0 @@
-var deferredPrompt=null;
-window.addEventListener("beforeinstallprompt",(e => {e.preventDefault(),deferredPrompt=e})),
-window.addEventListener("appinstalled",(()=>{deferredPrompt=null}));
-function addToDesktop(){deferredPrompt.prompt()}
